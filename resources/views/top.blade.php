a7344a24c8d22c8134ad49b0209af357c196ce7c
fuji <ko.fujishima8@gmail.com>
2022-02-01 09:18:36 +0000

    toppage

---
+++ resources/views/top.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.admin')
+@section('title', 'トップページ')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <h2>サイトトップ</h2>
+        </div>
+        <div class="row">
+            
+        </div>
+    </div>
+@endsection
